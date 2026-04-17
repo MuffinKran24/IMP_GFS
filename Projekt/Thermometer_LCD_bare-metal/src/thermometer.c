2dab9abc3e3c7acd4a22475d19b537f0872abf0d
MuffinKran24 <finn.brandt@gmx.de>
2026-04-17 22:53:30 +0200

    literally no progress made today

--- Projekt/Thermometer_LCD_bare-metal/src/thermometer.c
+++ Projekt/Thermometer_LCD_bare-metal/src/thermometer.c
@@ -1,15 +1,18 @@
 // Das Thermometer mit so wenigen Bibliotheken und so direkt wie möglich
 
+#include <stdio.h>
+#include <stdbool.h>
 #include <avr/io.h> // IO Funktionen aus der avr-controller Bibliothek importieren
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 int main(void) {
-  DDRB = DDRB | (1 << DDB5); 
 
+DDRB |= (1<<PB5);
+    
   while(1) {
-    PORTB = PORTB | (1 << PORTB5);
-    _delay_ms(500);    
-    PORTB = PORTB & ~(1 << PORTB5);
-    _delay_ms(500);
+    PORTB |= (1<<PB5);
+
+
   }
 }
