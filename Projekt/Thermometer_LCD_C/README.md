3350d9608d15c19a72924b24a2c753c281bd6043
MuffinKran24 <finn.brandt@gmx.de>
2026-06-18 23:01:07 +0200

    letzter commit

--- Projekt/Thermometer_LCD_C/README.md
+++ Projekt/Thermometer_LCD_C/README.md
@@ -1,11 +1,1 @@
 # Das direkte Beispiel funktioniert leider nicht auf Windows. Sorry :(
-
-# To-do
-- [ ] function to start lcd
-- [ ] function to write on lcd
-- [ ] function to read temperature or humidity from dht
-- [ ] make makefile universal
-
-```sh
-make all
-```
--- Projekt/Thermometer_LCD_ino/Thermometer_LCD/Thermometer_LCD.ino
+++ Projekt/Thermometer_LCD_ino/Thermometer_LCD/Thermometer_LCD.ino
@@ -25,8 +25,7 @@
 #define DHTTYPE DHT11 // DHT-Typ definieren, in diesem Fall ist es der DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
+
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
